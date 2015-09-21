8b16a62dadceadf60b2059db03a9a97d1c097061
Guest User <student@epicodus.com>
2015-09-21 10:37:16 -0700

    gonna add landmarks

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,37 @@ $(document).ready(function() {
                                  '</div>' +
                                '</div>');
   });//end of $("#add-address").click(function()
+  $("#add-landmark").click(function() {
+    $("#new-landmark").append('<div class="new-landmark">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-landmark-thingy">Landmark</label>' +
+                                   '<input type="text" class="form-control new-landmark-thingy">' +
+                                 '</div>'
+                                 );
+  });//end of $("#add-landmark").click(function()
+
   $("form#new-place").submit(function(event) {
     event.preventDefault();
     var inputtedLocation = $("input#new-location").val();
     var inputtedSpot = $("input#new-spot").val();
-    var newContact = { location: inputtedLocation, spot: inputtedSpot, addresses: [] };
+    var newContact = { location: inputtedLocation, spot: inputtedSpot, addresses: [], landmarks: [] };
     $(".new-address").each(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
       var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
       newContact.addresses.push(newAddress);
-    }); //end of $("form#new-place").submit(function(event)
+    }); //end of .new-address.each
+
+/*
+    //for each
+    $(".new-landmark").each(function() {
+        var inputtedLandmark = $(this).find("input.new-landmark-thingy").val();
+        var newLandmark = { landmark-name: inputtedLandmark };
+        newContact.landmarks.push(newLandmark);
+    });
+    */
+
     $("ul#places").append("<li><span class='place'>" + newContact.spot + "</span></li>");
     $(".place").last().click(function() {
       $("#show-place").show();
@@ -37,6 +56,15 @@ $(document).ready(function() {
       newContact.addresses.forEach(function(address) {
         $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
       });//end of newContact.addresses.forEach(function(address)
+
+/*
+      $("ul#landmarks").text("");
+      newContact.landmarks.forEach(function(landmark) {
+        $("ul#landmarks").append("<li>" + landmark.landmark-name + "</li>");
+      });
+
+      */
+
     }); //end of $(".place").last().click(function()
     $("input#new-location").val("");
     $("input#new-spot").val("");
